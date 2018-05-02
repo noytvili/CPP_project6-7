c7e2a0992e5f146523c764cc76b24b28aec6a7b0
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-02 21:39:28 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,13 +1,12 @@
-
 #include "Board.h"
 #include <iostream>
 using namespace std;
 
 Board:: Board(int n){ //constructor
     size = n;
-    board = new char*[size];  //allocBoard
+    board = new newChar*[size];  //allocBoard
 	for (int i = 0; i < size; i++){
-		board[i] = new char[size];
+		board[i] = new newChar[size];
         for (int j = 0; j < size; j++) {
             board[i][j] = '.';
         }
@@ -24,23 +23,50 @@ int Board:: getSize(){
     return size;
 }
 
- char Board::operator[](Pair &p) const {  //לקריאה בלבד
+ newChar Board::operator[](const Pair &p) const {  //לקריאה בלבד
     //cout << "c";
     return board[p.get_i()][p.get_j()];
 }
 
- char& Board::operator[](Pair &p) { //לשנות את הערך
-    if ((p.get_i() > size || p.get_i() < size) || (p.get_j() > size || p.get_j() < size)){
-        throw string("Illegal coordinate: "+to_string(p.get_i()) + to_string(p.get_j()));
+ newChar& Board::operator[](const Pair &p) { //לשנות את הערך
+    if ((p.get_i() >= size || p.get_i() < 0) || (p.get_j() >= size || p.get_j() < 0)){
+        throw IllegalCoordinateException(p);
     }
-    return board[p.get_i()][p.get_j()];
+        return board[p.get_i()][p.get_j()];
 }
 
 Board& Board::operator= (char c){
-    if(c!='X' || c!='O' || C!='.'){
-        throw IllegalCharException();
+     Board boradNew(*this);
+    if(c!='X' && c!='O' && c!='.'){
+        throw IllegalCharException(c);
     }
     else{
-        board[p.get_i()][p.get_j()]=c;
+	   delete[] board;
+       
+        for(int i = 0; i < size; i++)
+            for(int j = 0; j < size; j++)
+                boradNew.board[i][j] = c;
     }
+    this->board=boradNew.board;
+    return *this;
 }
+
+Board& Board::operator=(const Board& b1){
+     if (this==&b1){
+                return *this;
+     }
+        for (int i = 0; i < size; i++) //free
+	        delete[] board[i];
+	   delete[] board;
+    board = new newChar*[size];  //allocBoard
+	for (int i = 0; i < size; i++){
+		board[i] = new newChar[size];
+        for (int j = 0; j < size; j++) {
+            board[i][j] = b1.board[i][j];
+        }
+    }
+
+            return *this;
+        }
+    
+
