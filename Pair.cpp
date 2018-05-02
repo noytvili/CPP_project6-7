7b50e175ed860bc5390225bfb5e5a72176686e53
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-02 14:39:26 +0300

    Update Pair.cpp

--- Pair.cpp
+++ Pair.cpp
@@ -2,21 +2,18 @@
 #include "Pair.h"
 using namespace std;
 
-    
-    //  pair :: pair(){ //constuctor
-    //     i=0;
-    //     j=0;
-    // }
-    
 Pair :: Pair(int x, int y){ //constuctor
-    i=x;
-    j=y;
+    i = x;
+    j = y;
 }
-    
+
+Pair :: 
+
 int Pair :: get_i(){
     return i;
 }    
-    
+
+
 int Pair :: get_j(){
     return j;
 }      
@@ -25,4 +22,10 @@ void pair :: operator =(pair &p){
     i=p.get_i();
     j=p.get_j();
 
+}
+
+Pair& Pair:: operator= (const Pair& p){
+    this->i = p.get_i();
+    this->j = p.get_j();
+    return *this;
 }
