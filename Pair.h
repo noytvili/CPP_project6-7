21a330f9c065b31a59f388b5044c5b3583fc5a06
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-02 14:21:31 +0300

    Create Pair.h

---
+++ Pair.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Pair{
+    private:
+        int i;
+        int j;
+        
+    public:
+       // pair();
+        Pair(int x, int y);
+        int get_i();
+        int get_j();
+    
+};
+
+
+    
+    
