aa36d1a61678b8b841d1f5bf123e9d8d5b79bd9e
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-04 11:58:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "Board.h"
-
 #include <iostream>
 using namespace std;
 
@@ -25,6 +24,7 @@ int main() {
 	} catch (const IllegalCoordinateException& ex) {
 		cout << "Illegal coordinate: " << ex.theCoordinate() << endl;  // prints "Illegal coordinate: 3,4"
 	}
+	cout << board1[{3,2}] << endl;
 
 	board1 = '.';     // Fill the entire board with "."
 	cout << board1 << endl;  /* Shows an empty board, as above */
@@ -54,3 +54,4 @@ int main() {
 
 	return 0;
 }
+
