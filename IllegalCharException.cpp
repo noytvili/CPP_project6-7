62d65381fb9f5154cf75faa7288c63cb944c9af4
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-04 11:58:01 +0300

    Create IllegalCharException.cpp

---
+++ IllegalCharException.cpp
@@ -0,0 +1,18 @@
+#include <exception>
+using namespace std;
+
+
+class IllegalCharException{
+    private:
+      const  char c;
+    public:
+     IllegalCharException(const char c) : c(c){}
+    
+
+
+// Throw an object:
+char theChar() const {
+        return c;
+    }
+
+};
