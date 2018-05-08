1488bfeec71faaf4ee9f51f0ad3b564f12165ff4
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-08 13:48:38 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -15,23 +15,23 @@ class Board{
         newChar**board;
         Board(int n); //constructor
         Board(const Board& b1); // copyConstructor
-       // ~Board(); //destructor
-        int getSize();
+        int getSize() const;
          newChar operator[](const Pair &p) const;
          newChar& operator[](const Pair &p);
          Board& operator= (char c);
          Board& operator=(const Board& b1);
-         //char operator= (const Board& b1);
-
+         void clear();
+         ~Board(); //destructor
     
     //----------------------------------
     // friend global IO operators
     //----------------------------------
     //  friend istream& operator>> (istream& input,  Board& b);  // (cin)
+    friend ostream &operator<<(ostream &os, const Board& b);
         
 };
 
-    inline ostream &operator<<(ostream &os, Board b) {  //toString (cout)
+    inline ostream &operator<<(ostream &os, const Board& b) {  //toString (cout)
         for (int i = 0; i < b.getSize(); i++) {
             for (int j = 0; j < b.getSize(); j++) {
                 os << b.board[i][j];
