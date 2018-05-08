a335a3cfe711d42aa1d8d9d8f50582e5a913a4aa
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-08 13:48:18 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -11,23 +11,23 @@ Board:: Board(int n){ //constructor
 }
 
 Board:: Board(const Board& b1){ //copy constructor
+ int i,j;
     size = b1.size;
     board = new newChar*[size];  //allocBoard
-	for (int i = 0; i < size; i++){
+	for (i = 0; i < size; i++){
 		board[i] = new newChar[size];
-        for (int j = 0; j < size; j++) {
-            board[i][j] = b1.board[i][j];
-        }
-    }
+        for (j = 0; j < size; j++) 
+           // board[i][j] = b1.board[i][j];
+           board[i][j] = newChar(b1.board[i][j]);
+	}
+
 }
 
-
-int Board:: getSize(){
+int Board:: getSize() const{
     return size;
 }
 
  newChar Board::operator[](const Pair &p) const {  //לקריאה בלבד
-    //cout << "c";
     return board[p.get_i()][p.get_j()];
 }
 
@@ -39,33 +39,42 @@ int Board:: getSize(){
 }
 
 Board& Board::operator= (char c){
-     Board boradNew(*this);
     if(c!='X' && c!='O' && c!='.'){
         throw IllegalCharException(c);
     }
     else{
-	   delete[] board;
         for(int i = 0; i < size; i++)
             for(int j = 0; j < size; j++)
-                boradNew.board[i][j] = c;
+                board[i][j] = c;
     }
-    this->board=boradNew.board;
     return *this;
 }
 
 Board& Board::operator=(const Board& b1){
+    
      if (this==&b1){
             return *this;
      }
-        Board boradNew(b1);
-            this->board=boradNew.board;
 
-            return *this;
+    clear();
+    size = b1.size;
+    board = new newChar*[size];  //allocBoard
+	for (int i = 0; i < size; i++){
+		board[i] = new newChar[size];
+        for (int j = 0; j < size; j++) 
+            board[i][j] = b1.board[i][j];
+        
+    }
+    return *this;
         }
     
 
-Board:: ~Board(){  //distructor
+void Board:: clear(){  //distructor
     for (int i = 0; i < size; i++) //free
 	    delete[] board[i];
 	delete[] board;
+}
+
+Board:: ~Board(){  //distructor
+    clear();
 }
