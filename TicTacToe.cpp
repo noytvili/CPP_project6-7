717201d629ff0e0c35eefd1b85ea8e7b4df255dd
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-17 00:48:40 +0300

    Create TicTacToe.cpp

---
+++ TicTacToe.cpp
@@ -0,0 +1,21 @@
+//#include "Board.h"
+//#include "Player.h"
+#include "TicTacToe.h"
+    
+TicTacToe::TicTacToe(int n){
+    board.size=n;
+}
+
+const void TicTacToe:: play(Player player1, Player player2){
+    //לולאה עם המשחק ובדיקות ניצחון
+}
+    
+TicTacToe:: Player winner(){
+    //מחזיר את השחקן המנצח
+}
+
+TicTacToe:: Board board(){
+    //מחזירה את הלוח בסוף המשחק
+}
+
+    
