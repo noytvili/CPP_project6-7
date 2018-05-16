a0098ee16543da3e65e029c00a0a632ae4e39ea0
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-17 00:46:04 +0300

    Create Champion.h

---
+++ Champion.h
@@ -0,0 +1,9 @@
+#include "Player.h"
+#pragma once
+
+
+class Champion : public Player {
+    public:
+    const string name() const override;
+    const Coordinate play(const Board& board) override;
+}
