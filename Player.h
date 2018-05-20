57588b018c020310fa95b7d973ee118b32c27987
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-20 17:35:18 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,13 +2,12 @@
 #include "Board.h"
 #include <string>
 
-
 class Player {
-    
     public:
     char player_char;
 	virtual const string name() const =0;
 	virtual const Coordinate play(const Board& board)=0;
+
 	char getChar(){
 	    return player_char;
 	}
