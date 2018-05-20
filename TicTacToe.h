98d32154a822346dda0f8b2f346dab4d43656644
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-20 17:36:30 +0300

    Update TicTacToe.h

--- TicTacToe.h
+++ TicTacToe.h
@@ -2,11 +2,15 @@
 #include "Player.h"
 #pragma once
 
-class TicTacToe : public Player {
+class TicTacToe {
+    private:
+       Board* game_board;
+       Player* game_winner;
+       bool Winning(Player& player_X, Player& player_O);
     
     public:
         TicTacToe(int n);
-        const void TicTacToe:: play(Player player1, Player player2);
-         
-    
+        const void TicTacToe:: play(Player& player_X, Player& player_O);
+        const Player& winner() const;
+        Board& TicTacToe:: board() const;
 };
