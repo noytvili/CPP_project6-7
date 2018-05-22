25bf215a362e88d405c47b2dfacf6eb3fe854189
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-22 22:05:18 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -3,44 +3,38 @@
 #include "IllegalCoordinateException.cpp"
 #include "IllegalCharException.cpp"
 
-
 using namespace std;
-
 
 class Board{
     private :
-        int size;
-
+        int Size;
     public :
         newChar**board;
         Board(int n); //constructor
         Board(const Board& b1); // copyConstructor
-        int getSize() const;
+       // ~Board(); //destructor
+        int size();
+    
          newChar operator[](const Coordinate &p) const;
          newChar& operator[](const Coordinate &p);
          Board& operator= (char c);
          Board& operator=(const Board& b1);
-         bool operator==(const Board &b1) const;
-         void clear();
-         ~Board(); //destructor
-    
+         //char operator= (const Board& b1);
     //----------------------------------
+
     // friend global IO operators
+
     //----------------------------------
+
     //  friend istream& operator>> (istream& input,  Board& b);  // (cin)
-    friend ostream &operator<<(ostream &os, const Board& b);
-        
 };
 
-    inline ostream &operator<<(ostream &os, const Board& b) {  //toString (cout)
-        for (int i = 0; i < b.getSize(); i++) {
-            for (int j = 0; j < b.getSize(); j++) {
+    inline ostream &operator<<(ostream &os, Board b) {  //toString (cout)
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
                 os << b.board[i][j];
             }
                         os<<endl;
-
         }
         return os;
     }
-
- 
