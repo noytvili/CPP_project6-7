3c342761217ea1cfcf4d437fee8264208edc8f10
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-22 22:04:57 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 Board:: Board(int n){ //constructor
-    size = n;
+    Size = n;
     board = new newChar*[size];  //allocBoard
 	for (int i = 0; i < size; i++){
 		board[i] = new newChar[size];
@@ -11,82 +11,59 @@ Board:: Board(int n){ //constructor
 }
 
 Board:: Board(const Board& b1){ //copy constructor
- int i,j;
-    size = b1.size;
-    board = new newChar*[size];  //allocBoard
-	for (i = 0; i < size; i++){
-		board[i] = new newChar[size];
-        for (j = 0; j < size; j++) 
-           // board[i][j] = b1.board[i][j];
-           board[i][j] = newChar(b1.board[i][j]);
-	}
-
+    Size = b1.Size;
+    board = new newChar*[Size];  //allocBoard
+	for (int i = 0; i < Size; i++){
+		board[i] = new newChar[Size];
+        for (int j = 0; j < Size; j++) {
+            board[i][j] = b1.board[i][j];
+        }
+    }
 }
 
-int Board:: getSize() const{
-    return size;
+int Board:: size(){
+    return Size;
 }
 
  newChar Board::operator[](const Coordinate &p) const {  //לקריאה בלבד
+    //cout << "c";
     return board[p.get_i()][p.get_j()];
 }
 
  newChar& Board::operator[](const Coordinate &p) { //לשנות את הערך
-    if ((p.get_i() >= size || p.get_i() < 0) || (p.get_j() >= size || p.get_j() < 0)){
+    if ((p.get_i() >= Size || p.get_i() < 0) || (p.get_j() >= Size || p.get_j() < 0)){
         throw IllegalCoordinateException(p);
     }
         return board[p.get_i()][p.get_j()];
 }
 
 Board& Board::operator= (char c){
+     Board boradNew(*this);
     if(c!='X' && c!='O' && c!='.'){
         throw IllegalCharException(c);
     }
     else{
-        for(int i = 0; i < size; i++)
-            for(int j = 0; j < size; j++)
-                board[i][j] = c;
+	   delete[] board;
+        for(int i = 0; i < Size; i++)
+            for(int j = 0; j < Size; j++)
+                boradNew.board[i][j] = c;
     }
+    this->board=boradNew.board;
     return *this;
 }
 
 Board& Board::operator=(const Board& b1){
-    
      if (this==&b1){
             return *this;
      }
-
-    clear();
-    size = b1.size;
-    board = new newChar*[size];  //allocBoard
-	for (int i = 0; i < size; i++){
-		board[i] = new newChar[size];
-        for (int j = 0; j < size; j++) 
-            board[i][j] = b1.board[i][j];
-        
-    }
-    return *this;
+        Board boradNew(b1);
+            this->board=boradNew.board;
+            return *this;
         }
-    
-bool Board::operator==(const Board &b1) const {
-    if (size != b1.size)
-        return false;
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            if (board[i][j] != b1.board[i][j])
-                return false;
-        }
-    }
-    return true;
-}
 
-
-void Board:: clear(){  //distructor
-    for (int i = 0; i < size; i++) //free
-	    delete[] board[i];
-	delete[] board;
-}
 
 Board:: ~Board(){  //distructor
-    clear();
+    for (int i = 0; i < Size; i++) //free
+	    delete[] board[i];
+	delete[] board;
 }
