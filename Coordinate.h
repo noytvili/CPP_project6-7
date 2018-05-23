a1ee329775c7f08312bc08480e830b4896a8be95
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-23 17:45:41 +0300

    final

--- Coordinate.h
+++ Coordinate.h
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Coordinate{
     private:
-        int i;
-        int j;
+        uint i;
+        uint j;
         
     public:
        // pair();
-        Coordinate(int x, int y);
-        int get_i() const;
-        int get_j() const;
+        Coordinate(uint x, uint y);
+        uint get_i() const;
+        uint get_j() const;
         Coordinate& operator= (const Coordinate& p);
+        friend ostream& operator<< (ostream& os, const Coordinate& c);
     
 };
 
+    inline ostream& operator<< (ostream& os, const Coordinate& c){
+    return os << string(" ( " + to_string(c.get_i()) + "," + to_string(c.get_j()) + " ) " );
+    }
 
     
     
