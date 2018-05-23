ea1991f3204f349d782bc71e8a274f488a6ec205
noytvili <33618257+noytvili@users.noreply.github.com>
2018-05-23 19:09:58 +0300

    Delete Champion.h

--- Champion.h
+++
@@ -1,8 +0,0 @@
-#include "Player.h"
-#pragma once
-
-class Champion : public Player {
-    public:
-    const string name() const override;
-    const Coordinate play(const Board& board) override;
-};
