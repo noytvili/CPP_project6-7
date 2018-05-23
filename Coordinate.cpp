47abf657ea058b569d7f93b5ca0258c856fe80da
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-23 17:44:58 +0300

    final

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -2,26 +2,18 @@
 #include "Coordinate.h"
 using namespace std;
 
-    
-    //  pair :: pair(){ //constuctor
-    //     i=0;
-    //     j=0;
-    // }
-    
-Coordinate :: Coordinate(int x, int y){ //constuctor
+Coordinate :: Coordinate(uint x, uint y){ //constuctor
     i=x;
     j=y;
 }
     
-int Coordinate :: get_i() const{
+uint Coordinate :: get_i() const{
     return i;
 }    
     
-int Coordinate :: get_j() const{
+uint Coordinate :: get_j() const{
     return j;
 }      
-
-
 
 Coordinate& Coordinate:: operator= (const Coordinate& p){
     this->i = p.get_i();
