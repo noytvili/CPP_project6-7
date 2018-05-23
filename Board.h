e5fd8b4dbc4dc936e95bc646366b79da963606b0
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-23 17:42:39 +0300

    final

--- Board.h
+++ Board.h
@@ -8,33 +8,37 @@ using namespace std;
 class Board{
     private :
         int Size;
+
     public :
         newChar**board;
         Board(int n); //constructor
         Board(const Board& b1); // copyConstructor
-       // ~Board(); //destructor
-        int size();
-    
+        int size() const;
          newChar operator[](const Coordinate &p) const;
          newChar& operator[](const Coordinate &p);
          Board& operator= (char c);
          Board& operator=(const Board& b1);
-         //char operator= (const Board& b1);
+         bool operator==(const Board &b1) const;
+         void clear();
+         ~Board(); //destructor
+    
     //----------------------------------
-
     // friend global IO operators
-
     //----------------------------------
-
     //  friend istream& operator>> (istream& input,  Board& b);  // (cin)
+    friend ostream &operator<<(ostream &os, const Board& b);
+        
 };
 
-    inline ostream &operator<<(ostream &os, Board b) {  //toString (cout)
+    inline ostream &operator<<(ostream &os, const Board& b) {  //toString (cout)
         for (int i = 0; i < b.size(); i++) {
             for (int j = 0; j < b.size(); j++) {
                 os << b.board[i][j];
             }
                         os<<endl;
+
         }
         return os;
     }
+
+ 
