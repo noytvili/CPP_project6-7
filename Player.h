3ad60508c63345e3496a90d4190f8f266115736c
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-23 17:46:51 +0300

    final

--- Player.h
+++ Player.h
@@ -7,6 +7,7 @@ class Player {
     char myChar;
 	virtual const string name() const=0 ;
 	virtual const Coordinate play(const Board& board)=0;
+    void setChar(char c) {myChar = c;}
 
 	char getChar() const{
 	    return myChar;
