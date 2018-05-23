d7f11a7347b1020a5a3eec9bc64996b3e4e9ca13
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-05-23 14:51:00 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -1,32 +1,34 @@
 #include "Champion.h"
 
 const Coordinate Champion:: play(const Board& board){
-     newChar c = '.';
-     if(game_board[{0,0}] == c)  //left up
+     newChar c;
+     c.c= '.';
+   uint b_size= board.size();
+     if(board[{0,0}] == c)  //left up
           return Coordinate{0,0};
           
-     if(game_board[{game_board.size-1, game_board.size-1}] == c) //right down
-          return Coordinate{game_board.size-1, game_board.size-1};
+     if(board[{b_size-1, b_size-1}] == c) //right down
+          return Coordinate{b_size-1, b_size-1};
      
-     if(game_board[{0, game_board.size-1}] == c) // right up
-          return Coordinate{0, game_board.size-1};
+     if(board[{0, b_size-1}] == c) // right up
+          return Coordinate{0, b_size-1};
           
-     if(game_board[{game_board.size-1, 0}] == c) //left down
-          return Coordinate{game_board.size-1, 0};
+     if(board[{b_size-1, 0}] == c) //left down
+          return Coordinate{b_size-1, 0};
      
-     for (int i = 1; i < game_board.size; i++) { //left diagonal
-          if(game_board[{i,i}] == c)
+     for (uint i = 1; i < b_size; i++) { //left diagonal
+          if(board[{i,i}] == c)
                return Coordinate{i,i};
      }
      
-     for (int i = 1; i < game_board.size; i++) { //right diagonal
-          if(game_board[{i,game_board.size-1-i}] == c)
-               return Coordinate{i,game_board.size-1-i};
+     for (uint i = 1; i < b_size; i++) { //right diagonal
+          if(board[{i,b_size-1-i}] == c)
+               return Coordinate{i,b_size-1-i};
      }
      
-     for (int i = 0; i < game_board.size; i++) {
-          for (int i = 0; i < game_board.size; i++) {
-               if(game_board[{i,j}] == c)
+     for (uint i = 0; i < b_size; i++) {
+          for (uint j = 0; j < b_size; j++) {
+               if(board[{i,j}] == c)
                     return Coordinate{i,j};
           }
      }
